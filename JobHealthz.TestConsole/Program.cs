7db14ab5c1a0c8ecce8195dfe1f818c5ebc4a124
Ducas Francis <ducas@ducasfrancis.com>
2018-08-16 21:12:02 +1000

    add initial healthz module

---
+++ JobHealthz.TestConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace JobHealthz.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(l => l.AddConsole())
+                .AddSingleton<Healthz>()
+                .BuildServiceProvider();
+
+            serviceProvider.GetService<Healthz>()
+                .Start(() => new HealthzResult
+                {
+                    Status = r.Next(0, 10) >= 5 ? HealthzStatus.Ok : HealthzStatus.Fail,
+                    Message = r.Next(1, 10).ToString()
+                });
+            
+            Console.WriteLine("Press Enter to exit.");
+            try
+            {
+                Console.ReadLine();
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ JobHealthz/Healthz.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace JobHealthz
+{
+    public class Healthz
+    {
+        private readonly ILogger<Healthz> _logger;
+
+        public Healthz(ILogger<Healthz> logger)
+        {
+            _logger = logger;
+        }
+        public Healthz()
+        {
+            _logger = new NullLogger<Healthz>();
+        }
+        
+        public void Start(Func<HealthzResult> check)
+        {
+            var host =  new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Map("/healthz",
+                    a =>
+                    {
+                        a.Run(async context =>
+                        {
+                            _logger.LogTrace("Begin healthz check");
+                            var result = check();
+                            _logger.LogInformation("Healthz: {status} - {message}", result.Status, result.Message);
+                            context.Response.StatusCode = (int) result.Status;
+                            await context.Response.WriteAsync(result.Message);
+                            _logger.LogTrace("End healthz check");
+                        });
+                    })
+                )
+                .Build();
+            host.Start();
+            var urls = string.Join(",", host.ServerFeatures.Get<IServerAddressesFeature>().Addresses);
+            Console.WriteLine($"Health checks listening on {urls} at /healthz.");
+        }
+    }
+
+    public class HealthzResult
+    {
+        public HealthzStatus Status { get; set; } = HealthzStatus.Ok;
+        public string Message { get; set; } = string.Empty;
+    }
+
+    public enum HealthzStatus
+    {
+        Ok = 200,
+        Fail = 500
+    }
+}
